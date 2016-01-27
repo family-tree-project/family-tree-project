d997377e9f70ec513af9d83b30d85bb80957b321
Austin King <aust.a.king@gmail.com>
2016-01-27 15:11:47 -0800

    Can now add a new member and specify one parent.

--- routes/family_tree_routes.js
+++ routes/family_tree_routes.js
@@ -50,29 +50,37 @@ var queries = function() {
     + "deathCoords: {deathCoords},"
     + "nodeSize: 5";  //arbitrary field that the rendering engine needs
 
+  var unspecified_params = "name: 'not specified', nodeSize: 3";
+
   return {
+    //When specifying both parents, find their joined offspring node
     findParents:
       "MATCH (p1)-[]->(onode)<-[]-(p2) "
-      + "WHERE id(p1)={parent1} AND id(p2)={parent2} "
-      + "RETURN onode",
+        + "WHERE id(p1)={parent1} AND id(p2)={parent2} "
+        + "RETURN onode",
     findFamily:
       "MATCH (p:Person)-[*0..]-(:User {username: {username}}) RETURN p",
+    //When only one parent is specified, create an unspecified parent and new offspring node
+    createNodeWithOneParent:
+      "MATCH (p) WHERE id(p)={parent} "
+        + "CREATE (p)-[:PARENTED]->(o:Offspring)-[:CHILD]->(:Person {" + node_params + "}) "
+        + "CREATE (:Person {" + unspecified_params + "})-[:PARENTED]->(o)",
+    //When specifying both parents, the new node is pointed to by their joined offspring node
     createNodeWithParents:
       "MATCH (onode) WHERE id(onode)={offspringNodeID} "
         + "CREATE (:Person {" + node_params
         + "})<-[:CHILD]-(onode)",
+    //When specifying child(ren) and no parents exist, create a new offspring node and unspecified parent
     createNodeWithChild:
       "MATCH (child) WHERE id(child)={childNodeID} "
         + "CREATE (:Person {" + node_params
         + "})-[:PARENTED]->(offspringNode:Offspring {nodeSize: 1})-[:CHILD]->(child) "
-        + "CREATE (:Person {name: 'Not Specified', nodeSize: 3})-[:PARENTED]->(offspringNode) "
+        + "CREATE (:Person {" + unspecified_params + "})-[:PARENTED]->(offspringNode) "
         + "RETURN offspringNode"
   }
 }();
 
 familyTreeRouter.post('/tree', jsonParser, authenticat.tokenAuth, function(req, res) {
-  //User will give name, birthDate, birthLoc, deathDate, deathLoc, parents and/or children (by id)
-  //When specifying parents, both must exist (for now) to find their offspring node
   if(req.body.parents.length === 2) {
     db.cypherQuery(queries.findParents,
       {
@@ -105,8 +113,27 @@ familyTreeRouter.post('/tree', jsonParser, authenticat.tokenAuth, function(req, 
       }
     );
   }
+  else if(req.body.parents.length === 1) {
+    db.cypherQuery(queries.createNodeWithOneParent,
+      {
+        name: req.body.name,
+        birthDate: req.body.birthDate,
+        birthLoc: req.body.birthLoc,
+        birthCoords: req.body.birthCoords,
+        deathDate: req.body.deathDate || '',
+        deathLoc: req.body.deathLoc || '',
+        deathCoords: req.body.deathCoords || '',
+        parent: req.body.parents[0]
+      },
+      function(err, result) {
+        if(err) {
+          return handleError(err, res, 500);
+        }
 
-  //When specifying child(ren) and no parents exist, a new offspring node and unspecified parent need to be created.
+        return res.json({msg: 'Member added'});
+      }
+    );
+  }
   else if(req.body.children.length) {
     db.cypherQuery(queries.createNodeWithChild,
     {
@@ -134,7 +161,7 @@ familyTreeRouter.put('/tree', jsonParser, authenticat.tokenAuth, function(req, r
   db.readNode(req.body.id, function(err, node) {
     db.updateNode(req.body.id,
       {
-        //db.updateNode replaces all node properties, so all fields need to be assigned to avoid losing any
+        //db.updateNode replaces the node while keeping its ID, so all fields need to be assigned to avoid losing any
         name: req.body.name || node.name,
         birthDate: req.body.birthDate || node.birthDate,
         birthLoc: req.body.birthLoc || node.birthLoc,
@@ -142,7 +169,7 @@ familyTreeRouter.put('/tree', jsonParser, authenticat.tokenAuth, function(req, r
         deathDate: req.body.deathDate || node.deathDate,
         deathLoc: req.body.deathLoc || node.deathLoc,
         birthCoords: req.body.birthCoords || node.birthCoords,
-        nodeSize: 5 //if this was previously a 'Not Specified' person, make its new size equal to a real person
+        nodeSize: 5 //if this was previously a 'not specified' person, make its new size equal to a real person
       },
       function(err, result) {
         if(err) {
